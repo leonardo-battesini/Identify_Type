eb07ac34189b416beb290378623ff9d06e83e327
Leonardo Battesini <leonardoffline@gmail.com>
2022-04-15 01:04:33 -0300

    começo da função

---
+++ identify_type.py
@@ -0,0 +1,162 @@
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import re
+
+def isDate(value):
+    try: ### 22/05/2003
+        date = datetime.strptime(value, '%d/%m/%Y').date()
+        return(1)
+    except:
+        try: ### 22-05-2003
+            date = datetime.strptime(value, '%d-%m-%Y').date()
+            return(2)
+        except:
+            try: ### 2003-05-22
+                date = datetime.strptime(value, '%Y-%m-%d').date()
+                return(3)
+            except:
+                try: ### 2003/05/22
+                    date = datetime.strptime(value, '%Y/%m/%d').date()
+                    return(4)
+                except:
+                    return(0)
+
+def isNumberFlo(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    return True
+
+def convertStrToStrNumber(text):
+    text = str(text)
+    text = re.sub('[^0-9]', '', text)
+    return str(text)
+
+def convertColumn(type, column):
+    if type == 'digit':
+        column = column.fillna(0)
+        column = column.convert_dtypes(convert_integer=True)
+    elif type == 'flo':
+        column = column.fillna(0)
+        column = column.convert_dtypes(convert_floating=True)
+    elif type == 'nul':
+        column = column.fillna('')
+        column = column.convert_dtypes(convert_string=True)
+    elif type == 'empt':
+        column = column.fillna('')
+        column = column.convert_dtypes(convert_string=True)
+    elif type == 'date':
+        column = column.convert_dtypes(convert_string=True)
+        column = column.fillna(np.nan)
+    elif type == 'txt':
+        column = column.fillna('')
+        column = column.convert_dtypes(convert_string=True)
+    return column
+
+def trataColumn(column):
+    data_types = {}
+
+    for value in column:
+        if str(value) == 'nan':                        #if null
+            if 'nul' not in data_types.keys():
+                data_types['nul'] = 1
+            else:
+                data_types['nul'] += 1
+                
+        elif str(value).replace(" ", "").isdigit():    #if digit
+            if 'digit' not in data_types.keys():
+                data_types['digit'] = 1
+            else:
+                data_types['digit'] += 1
+            
+        elif isNumberFlo(str(value).replace(" ", "")): #if float
+            if 'flo' not in data_types.keys():
+                data_types['flo'] = 1
+            else:
+                data_types['flo'] += 1
+                
+        elif len(value.replace(" ", "")) == 0:         #if empty
+            if 'empt' not in data_types.keys():
+                data_types['empt'] = 1
+            else:
+                data_types['empt'] += 1
+
+        elif isDate(str(value)):                       #if date
+            if 'date' not in data_types.keys():
+                data_types['date'] = 1
+            else:
+                data_types['date'] += 1
+                
+        else:                                          #if not treated
+            if 'txt' not in data_types.keys():
+                data_types['txt'] = 1
+            else:
+                data_types['txt'] += 1
+
+    max_type = max(data_types, key=data_types.get)
+
+    if len(data_types) == 1:
+        return(convertColumn(max_type, column))
+    else:
+        try:
+            del data_types['nul']
+        except:
+            try:
+                del data_types['empt']
+            except:
+                1==2
+        
+        if len(data_types) >= 2:
+            try:
+                del data_types['empt']
+            except:
+                1==2
+        
+        if len(data_types) == 1:
+            return(convertColumn(max_type, column))
+        
+        if max_type == 'txt':
+            return(convertColumn(max_type, column))
+        
+        if max_type == 'digit':
+            for i in range(column.size):
+            #for i in column:
+                if not str(column[i]).replace(" ", "").isdigit():
+                    column[i] = column[i].replace(" ", "")
+                    column[i] = convertStrToStrNumber(column[i])
+
+            return(convertColumn(max_type, column))
+        
+        print('VERIFICAR O TRATAMENTO!! ', data_types, column.iloc[:10])
+        return(convertColumn(max_type, column))
+
+def trataDf(df):
+    columns = list(df.columns)
+
+    for column in columns:
+        df[column] = trataColumn(df[column])
+    return df
+
+
+
+
+
+#print(os.listdir())
+df = pd.read_csv("util_convert_column/cabeca.csv")
+
+#values = df.iloc[:,2]
+
+#df.iloc[:,0:]
+
+#print(list(df.dtypes))
+#df.to_csv('1.csv')
+
+df = trataDf(df)
+
+print(df)
+
+#print(list(df.dtypes))
+
+df.to_csv('util_convert_column/2.csv')
